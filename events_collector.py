363177fe32b8bd9f26065b9daa0abfff02950c83
Raul <rgomezbe@cisco.com>
2023-04-24 00:11:29 -0600

    updates for events collector

--- events_collector.py
+++ events_collector.py
@@ -1,76 +1,74 @@
-from modules.parser import load_yaml_config
 from modules.vmanage import sdwan_manager
-import json
-from datetime import datetime
-from pytz import timezone
-import csv
+from modules.eventsParser import (
+    load_yaml_config,
+    get_arguments,
+    save_csv,
+    parse_filter_rules,
+    create_filename,
+    get_all_event_names,
+    get_all_devices,
+    get_all_components,
+    get_table,
+    )
 
-def convert_epoc_to_pst(epoctime):
-    date = datetime.fromtimestamp(epoctime, timezone('US/Central'))
-    parsed_date = f'{str(date)[:-6]} CST'
-    return parsed_date
 
 if __name__ == '__main__':
 
-    var = load_yaml_config('config/config.yaml')
+    try:
 
-    session = sdwan_manager(
-        var['VMANAGE'],
-        str(var['PORT']),
-        var['USERNAME'],
-        var['PASSWORD'])
+        args = get_arguments()
+        var = load_yaml_config('config/config.yaml')
 
-    rules_set = [
-        #{"value":["sla-change"], "field":"eventname", "type":"string", "operator": "in"},
-        {"value":["168"], "field":"entry_time", "type":"string", "operator":"last_n_hours"}
-        ]
-    
-    payload = json.dumps({"query": {"condition": "AND","rules": rules_set},"size": 10000})
+        host = var['VMANAGE']
+        port = str(var['PORT'])
+        user = var['USERNAME']
+        pwd = var['PASSWORD']
+        session = sdwan_manager(host,port,user,pwd)
 
-    response = session.send_request(action='POST',resource=f'/event',body=payload)
+        if args.showdevices:
 
-    data = response.json()["data"]
+            response = session.send_request(action='GET',resource=f'/device',body={})
+            all_devices = get_all_devices(response)
+            print(get_table(all_devices,headers=['SYSTEM IP', 'HOSTNAME'],alignment='center'))
+            session.logout()
 
-    with open ('testfile.csv','w') as f:
+        elif args.showevents:
+            response = session.send_request(action='GET',resource=f'/event/types/keyvalue',body={})
+            all_events = get_all_event_names(response)
+            print(get_table(all_events,headers=['AVAILABLE EVENT NAMES'],alignment='left'))
 
-        csv_writer = csv.writer(f)
-        csv_writer.writerow(['Event Time', 'Hostname', 'System IP', 'Name', 'Severity', 'Component', 'Details'])
-        all_events = []
-        for event in data:
-            startTime = str(event['entry_time'])[:-3]
-            event["entry_time"] = convert_epoc_to_pst(int(startTime))
-            new_row = [
-                event["entry_time"],
-                event["host_name"],
-                event["system_ip"],
-                event["eventname"],
-                event["severity_level"],
-                event["component"],
-                event["details"],
-            ]
-
-            csv_writer.writerow(new_row)
-            all_events.append(new_row)
-
-
-names = ["bgp-peer-state-change",
-"dhcp-address-renewed",
-"sla-change",
-"control-connection-tloc-ip-change",
-"system-logout-change",
-"system-login-change",
-"interface-state-change",
-"fib-updates",
-"sla-violation",
-"fib-update",
-"system-aaa-login-fail",
-"bfd-state-change"
-]
-
-for name in names:
-    x = 0
-    for event in all_events:
-        if name in event:
-            x += 1
+        elif args.showcomponents:
+            response = session.send_request(action='GET',resource=f'/event/component/keyvalue',body={})
+            all_components = get_all_components(response)
+            print(get_table(all_components,headers=['AVAILABLE COMPONENT NAMES'],alignment='left'))
         
-    print(name, ':',x)
+        else:
+
+            eventnames = args.eventnames.split(',') if args.eventnames else None
+            devices = args.devices.split(',') if args.devices else None
+            severities = args.severities.split(',') if args.severities else None
+            components = args.components.split(',') if args.components else None
+
+            hours = [args.hours]
+            max_results = args.maxresults
+
+            payload = parse_filter_rules(
+                devices,
+                eventnames,
+                severities,
+                hours,
+                max_results,components)
+
+            response = session.send_request(action='POST',resource=f'/event',body=payload)
+
+            data = response.json()["data"]
+
+            filename = create_filename()
+
+            save_csv(filename,data)
+    
+            session.logout()
+
+    except Exception as error:
+        print('Execution Error')
+        print(error)
---
+++ modules/eventsParser.py
@@ -0,0 +1,135 @@
+import yaml
+import json
+import csv
+import sys
+import os
+from pytz import timezone
+from datetime import datetime
+from argparse import ArgumentParser
+from tabulate import tabulate
+from getpass import getpass
+
+
+def parse_filter_rules(devices,eventnames,severity,hours,max_results,components):
+
+    device_rule = {"value":devices, "field":"system_ip", "type":"string", "operator": "in"}
+    event_rule = {"value":eventnames, "field":"eventname", "type":"string", "operator": "in"}
+    component_rule = {"value":components, "field":"component", "type":"string", "operator": "in"}
+    severity_rule = {"value":severity, "field":"severity", "type":"string", "operator":"in"}
+    hours_rule = {"value":hours, "field":"entry_time", "type":"string", "operator":"last_n_hours"}
+
+    rules_set = []
+
+    if devices: rules_set.append(device_rule)
+    if eventnames: rules_set.append(event_rule)
+    if components: rules_set.append(component_rule)
+    if severity: rules_set.append(severity_rule)
+    if hours: rules_set.append(hours_rule)
+
+    payload = json.dumps({"query": {"condition": "AND","rules": rules_set},"size": max_results})
+
+    return payload
+
+
+def save_csv(filename,data):
+
+    if not data:
+        print('No data found for given filters!')
+        sys.exit(1)
+
+    with open (filename,'w') as f:
+
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(['Event Time', 'Hostname', 'System IP', 'Name', 'Severity', 'Component', 'Details'])
+        all_events = []
+        for event in data:
+            startTime = str(event['entry_time'])[:-3]
+            event["entry_time"] = convert_epoc_to_tz(int(startTime),'US/Central','CST')
+            new_row = [
+                event["entry_time"],
+                event["host_name"],
+                event["system_ip"],
+                event["eventname"],
+                event["severity_level"],
+                event["component"],
+                event["details"],
+            ]
+
+            csv_writer.writerow(new_row)
+            all_events.append(new_row)
+
+    print('File saved as:',filename)
+
+
+def convert_epoc_to_tz(epoctime,tz,acronym):
+    date = datetime.fromtimestamp(epoctime, timezone(tz))
+    parsed_date = f'{str(date)[:-6]} {acronym}'
+    return parsed_date
+
+
+def get_arguments():
+
+    parser = ArgumentParser(description='Arguments to filter collected events')
+    parser.add_argument('--devices', default=None, help='System IP to filter, if multiple separate with commas')
+    parser.add_argument('--eventnames', default=None, type=str, help='Event name to filter, if multiple separate with comma')
+    parser.add_argument('--components', default=None, type=str, help='Components name to filter, if multiple separate with comma')
+    parser.add_argument('--hours', default="24", type=str, help='Filter the events based on the last N hours 1 day = 24 ; 1 week = 168')
+    parser.add_argument('--severities', default=None, help='Severity to filter, if multiple separate with commas [Critical, Major, , Medium, Minor]')
+    parser.add_argument('--maxresults', default=10000, type=int, help='Maximum number of results to collect')
+    parser.add_argument('--showdevices', action="store_true", help="Print available devices system IPs on terminal")
+    parser.add_argument('--showevents', action="store_true", help="Print available event names on terminal")
+    parser.add_argument('--showcomponents', action="store_true", help="Print available components system IPs on terminal")
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+def load_yaml_config(config_file):
+
+    with open(config_file, 'r') as f:
+        variables = yaml.safe_load(f)
+
+    for key in variables.keys():
+        if not variables[key]:
+            if key == 'PASSWORD':
+                variables[key] = getpass(f'{key}: ')
+            else:
+                variables[key] = input(f'{key}: ')
+
+    return variables
+
+
+def create_filename():
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    modules = os.path.dirname(__file__)
+    outputs = os.path.join(os.path.dirname(modules), "outputs")
+    filename = os.path.join(outputs, f'VMANAGE-EVENTS--{timestamp}.csv')
+    return filename
+
+
+def get_all_devices(response):
+
+    all_devices = []
+    for device in response.json()['data']:
+        if device['personality'] not in ['vmanage','vsmart','vbond']:
+            all_devices.append([device['system-ip'],device['host-name']])
+
+    return all_devices
+
+
+def get_all_components(response):
+
+    all_components = [[line['value']] for line in response.json()['data']]
+
+    return all_components
+
+
+def get_all_event_names(response):
+
+    all_events = [[line['value']] for line in response.json()['data']]
+
+    return all_events
+
+
+def get_table(list,headers,alignment='center'):
+    return tabulate(list,headers=headers,tablefmt='pretty',colalign=({alignment},))
